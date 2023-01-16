0a69706e92c2ebb2b5a7f05cd87091544ea6ca24
jkatic85 <jovan.katic@prodigygame.com>
2023-01-16 13:44:55 -0500

    Refactor Day 3 part 1

--- day-3/day-3.go
+++ day-3/day-3.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -16,6 +17,8 @@ func main() {
 
 	fileScan := bufio.NewScanner(readFile)
 	sum := 0
+	sum1 := 0
+	sum2 := 0
 
 	for fileScan.Scan() {
 		item := fileScan.Text()
@@ -23,138 +26,48 @@ func main() {
 		p2 := item[len(item)/2:]
 		fmt.Println(p1 + " " + p2)
 		rep := 0
-		for _, r := range p1 {
-			c1 := string(r)
-			fmt.Printf("Checking: %s\n", c1)
-			for _, s := range p2 {
-				c2 := string(s)
-				if c1 == c2 && rep == 0 {
-					fmt.Printf("Found a duplicate: %s! \n", c1)
-					sum += prioritySum(c2)
+		rep1 := 0
+		for _, r1 := range p1 {
+			fmt.Printf("Checking: %c\n", r1)
+			// strings.Contains
+			isTrue := strings.Contains(p2, string(r1))
+			if isTrue && rep1 == 0 {
+				fmt.Printf("strings.Contains found a duplicate: %c! \n", r1)
+				sum += priority(r1)
+				rep1++
+			}
+			for _, r2 := range p2 {
+				if r1 == r2 && rep == 0 {
+					fmt.Printf("Second for loop found a duplicate: %c! \n", r1)
+					sum1 += priority(r2)
+					sum2 += priorityChar(r2)
 					rep++
 					break
 				}
 			}
 		}
 	}
-	fmt.Println(sum)
+	fmt.Println(sum, sum1, sum2)
 }
 
-func prioritySum(l string) int {
-	x := 0
-	switch l {
-	case "a":
-		x = 1
-	case "b":
-		x = 2
-	case "c":
-		x = 3
-	case "d":
-		x = 4
-	case "e":
-		x = 5
-	case "f":
-		x = 6
-	case "g":
-		x = 7
-	case "h":
-		x = 8
-	case "i":
-		x = 9
-	case "j":
-		x = 10
-	case "k":
-		x = 11
-	case "l":
-		x = 12
-	case "m":
-		x = 13
-	case "n":
-		x = 14
-	case "o":
-		x = 15
-	case "p":
-		x = 16
-	case "q":
-		x = 17
-	case "r":
-		x = 18
-	case "s":
-		x = 19
-	case "t":
-		x = 20
-	case "u":
-		x = 21
-	case "v":
-		x = 22
-	case "w":
-		x = 23
-	case "x":
-		x = 24
-	case "y":
-		x = 25
-	case "z":
-		x = 26
-	case "A":
-		x = 27
-	case "B":
-		x = 28
-	case "C":
-		x = 29
-	case "D":
-		x = 30
-	case "E":
-		x = 31
-	case "F":
-		x = 32
-	case "G":
-		x = 33
-	case "H":
-		x = 34
-	case "I":
-		x = 35
-	case "J":
-		x = 36
-	case "K":
-		x = 37
-	case "L":
-		x = 38
-	case "M":
-		x = 39
-	case "N":
-		x = 40
-	case "O":
-		x = 41
-	case "P":
-		x = 42
-	case "Q":
-		x = 43
-	case "R":
-		x = 44
-	case "S":
-		x = 45
-	case "T":
-		x = 46
-	case "U":
-		x = 47
-	case "V":
-		x = 48
-	case "W":
-		x = 49
-	case "X":
-		x = 50
-	case "Y":
-		x = 51
-	case "Z":
-		x = 52
+//  Array approach:
+// 		Range over a string adding a index +1 value
+const str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func priority(r rune) int {
+	return strings.Index(str, string(r)) + 1
+}
+
+// Numeric approach:
+//		Get utf-8 char code for each letter and convert it to a number between 1 - 52
+func priorityChar(r rune) int {
+	if r < rune(97) {
+		return int((r - 38))
+	} else {
+		return int((r - 96))
 	}
-	return x
 }
 
-// func prioritySum(string) int{
-// 	value1 := [range(abcdefghijklmnopqrstuvwxyz): range(1,26)]
-// // I'm dumb...
-// 	value2 = dict(zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ", range(27,52)))
-// 	sum := value1 + value2
-// 	return sum
-// }
+// to-do create func that finds duplicate item in rucksack
+// it returns the value of the item
+// it does not continue iterating after finding the item
