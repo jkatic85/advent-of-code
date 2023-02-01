b305759e8d316f3ec3f67418b9a0f7f8681067a7
jkatic85 <jovan.katic@prodigygame.com>
2023-02-01 11:37:25 -0500

    Refactor and tidy up

--- day-4/day-4.go
+++ day-4/day-4.go
@@ -17,30 +17,20 @@ func main() {
 	defer readFile.Close()
 
 	fileScan := bufio.NewScanner(readFile)
-	split := []string{}
 	sum := 0
 	ol := 0
 	for fileScan.Scan() {
 		item := fileScan.Text()
-		// Splits strings based on ","
-		split = strings.Split(item, ",")
-		// fmt.Println(split)
+		split := strings.Split(item, ",")
 		p1 := split[:1]
 		p2 := split[1:]
 		sp1 := splitter(p1)
 		sp2 := splitter(p2)
-		fmt.Println(sp1, sp2)
-		fmt.Println("------------")
-		fmt.Println(checker(sp1, sp2))
-		fmt.Println("----")
-		fmt.Println(overlap(sp1, sp2))
-		fmt.Println("============")
-		sum += checker(sp1, sp2)
-		ol += overlap(sp1, sp2)
+		sum += fullOverlap(sp1, sp2)
+		ol += partialOverlap(sp1, sp2)
 	}
-	fmt.Printf("Found %d duplicates.\n", sum)
-	fmt.Printf("Found %d overlaps.\n", ol)
-
+	fmt.Printf("Found %d full overlaps.\n", sum)
+	fmt.Printf("Found %d partial overlaps.\n", ol)
 }
 
 // Splits string arrays based on "-"
@@ -56,35 +46,17 @@ func splitter(split []string) []int {
 }
 
 // Finds if it overlaps at all
-func overlap(a, b []int) int {
-	firstA := a[0]
-	lastA := a[1]
-	firstB := b[0]
-	lastB := b[1]
-	c := 0
-	if firstA >= firstB && firstA <= lastB {
-		c++
-	} else if firstB >= firstA && firstB <= lastA {
-		c++
-	} else {
-		c += 0
+func partialOverlap(a, b []int) int {
+	if (a[0] >= b[0] && a[0] <= b[1]) || (b[0] >= a[0] && b[0] <= a[1]) {
+		return 1
 	}
-	return c
+	return 0
 }
 
 // Finds full overlaps
-func checker(a, b []int) int {
-	firstA := a[0]
-	lastA := a[1]
-	firstB := b[0]
-	lastB := b[1]
-	c := 0
-	if firstA >= firstB && lastA <= lastB {
-		c++
-	} else if firstB >= firstA && lastB <= lastA {
-		c++
-	} else {
-		c += 0
+func fullOverlap(a, b []int) int {
+	if (a[0] >= b[0] && a[1] <= b[1]) || (b[0] >= a[0] && b[1] <= a[1]) {
+		return 1
 	}
-	return c
+	return 0
 }
